2a54044a737adb766ffa79102f1681358872fa33
Phillip Lewis <phillip.lewisjr@gmail.com>
2017-03-26 15:46:04 -0500

    only needed the app.use

--- public/app.js
+++ public/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 
+//using public folder
 app.use(express.static("public"));
 
-app.get('/', (res, req)=>{
-  res.send()
-});
+
+// app.get('/', (res, req)=>{
+//   res.send()
+// });
 
 app.listen(3000, ()=> {
   console.log("Listening on 3000")
